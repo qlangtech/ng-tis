c9b3847bd3da67dd0a95d6c1a6cb4a52ea7e0b8c
莫正华 <mozhenghua@mozhenghuadeMacBook-Pro.local>
2020-08-08 12:57:40 +0800

    fix environments

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from "./runtime/app.module-jit";
-import { enableProdMode } from '@angular/core';
+import {enableProdMode} from '@angular/core';
 
 // import 'zone.js';
 // import 'reflect-metadata';
-import { environment } from './environments/environment';
-
+import {environment} from './environments/environment';
+//
 if (environment.production) {
   enableProdMode();
 }
