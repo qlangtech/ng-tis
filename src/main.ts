3894bbc2955bda23e5e80b3c5738275eba0acd3a
莫正华 <mozhenghua@mozhenghuadeMacBook-Pro.local>
2020-08-09 09:36:13 +0800

    modify main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from "./runtime/app.module-jit";
 import {enableProdMode} from '@angular/core';
 
-// import 'zone.js';
-// import 'reflect-metadata';
 import {Tis} from './environments/environment';
 //
 if (Tis.environment.production) {
