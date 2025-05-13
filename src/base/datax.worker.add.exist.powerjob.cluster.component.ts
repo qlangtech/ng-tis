a31eb70b2a05f98d1c83ed84424a553661a33398
mozhenghua <baisui@qlangtech.com>
2025-05-13 12:19:58 +0800

    fix bug for dataflow parser executor for powerjob distribute mode https://github.com/datavane/tis/issues/442

--- src/base/datax.worker.add.exist.powerjob.cluster.component.ts
+++ src/base/datax.worker.add.exist.powerjob.cluster.component.ts
@@ -34,104 +34,120 @@ import {PowerjobCptType} from "./base.manage-routing.module";
 
 
 @Component({
-    template: `
-        <tis-steps [type]="this.dto.processMeta.stepsType" [step]="0"></tis-steps>
-        <tis-page-header [showBreadcrumb]="false">
-            <tis-header-tool>
-                <button nz-button nzType="default" (click)="prestep()">上一步</button>&nbsp;
-                <button nz-button nzType="primary" (click)="createStep1Next()">下一步</button>
-            </tis-header-tool>
-        </tis-page-header>
-        <nz-spin [nzSpinning]="this.formDisabled">
-            <div class="item-block">
-                <tis-plugins [formControlSpan]="20" [pluginMeta]="[pluginCategory]"
-                             (afterSave)="afterSaveReader($event)" [savePlugin]="savePlugin" [showSaveButton]="false"
-                             [shallInitializePluginItems]="false" [_heteroList]="dto.powderJobUseExistClusterHetero"
-                             #pluginComponent></tis-plugins>
-            </div>
-        </nz-spin>
+  template: `
+    <tis-steps [type]="this.dto.processMeta.stepsType" [step]="0"></tis-steps>
+    <tis-page-header [showBreadcrumb]="false">
+      <tis-header-tool>
+        <button nz-button nzType="default" (click)="prestep()">上一步</button>&nbsp;
+        <button nz-button nzType="primary" (click)="createStep1Next()">下一步</button>
+      </tis-header-tool>
+    </tis-page-header>
+    <nz-spin [nzSpinning]="this.formDisabled">
+      <div class="item-block">
+        <tis-plugins [formControlSpan]="20" [pluginMeta]="[pluginCategory]"
+                     [savePluginEventCreator]="_savePluginEventCreator"
+                     (afterSave)="afterSaveReader($event)" [savePlugin]="savePlugin" [showSaveButton]="false"
+                     [shallInitializePluginItems]="false" [_heteroList]="dto.powderJobUseExistClusterHetero"
+                     #pluginComponent></tis-plugins>
+      </div>
+    </nz-spin>
 
-    `
+  `
 })
 export class DataxWorkerAddExistPowerjobClusterComponent extends BasicFormComponent implements AfterViewInit, OnInit {
-    // hlist: HeteroList[] = [];
-    savePlugin = new EventEmitter<SavePluginEvent>();
-    @Input() dto: DataxWorkerDTO;
-    @Output() nextStep = new EventEmitter<any>();
-    @Output() preStep = new EventEmitter<any>();
-    pluginCategory: PluginType = {name: 'datax-worker', require: true,extraParam:EXTRA_PARAM_DATAX_NAME+ PowerjobCptType.Server};
+  // hlist: HeteroList[] = [];
+  savePlugin = new EventEmitter<SavePluginEvent>();
+  @Input() dto: DataxWorkerDTO;
+  @Output() nextStep = new EventEmitter<any>();
+  @Output() preStep = new EventEmitter<any>();
+  pluginCategory: PluginType
+    = {
+    name: 'datax-worker'
+    , require: true
+    , extraParam: EXTRA_PARAM_DATAX_NAME + PowerjobCptType.Server
+  };
+  _savePluginEventCreator: () => SavePluginEvent;
+  constructor(tisService: TISService, modalService: NzModalService) {
+    super(tisService, modalService);
+  }
 
-    constructor(tisService: TISService, modalService: NzModalService) {
-        super(tisService, modalService);
+  // get currentApp(): CurrentCollection {
+  //   return new CurrentCollection(0, this.dto.processMeta.targetName);
+  // }
+  createStep1Next() {
+
+    let e = this.createSavePluginEvent();
+    // new SavePluginEvent();
+    // e.notShowBizMsg = true;
+    // e.overwriteHttpHeaderOfAppName(this.dto.processMeta.targetName);
+    // e.serverForward = "coredefine:datax_action:save_datax_worker";
+    // e.postPayload = {"k8sSpec": this.dto.powderJobServerRCSpec};
+    //  let appTisService: TISService = this.tisService;
+    //  appTisService.currentApp = new CurrentCollection(0, );
+    //  e.basicModule = this;
+    this.savePlugin.emit(e);
+  }
+
+  createSavePluginEvent(): SavePluginEvent {
+    let e = new SavePluginEvent();
+    e.notShowBizMsg = true;
+    e.overwriteHttpHeaderOfAppName(this.dto.processMeta.targetName);
+    return e;
+  }
+
+  protected initialize(app: CurrentCollection): void {
+  }
+
+  ngAfterViewInit() {
+  }
+
+
+  ngOnInit(): void {
+    this._savePluginEventCreator = () => {
+      return this.createSavePluginEvent();
+    }
+    if (this.dto.containPowerJob) {
+      return;
     }
 
-    // get currentApp(): CurrentCollection {
-    //   return new CurrentCollection(0, this.dto.processMeta.targetName);
-    // }
-    createStep1Next() {
+    this.httpPost('/coredefine/corenodemanage.ajax'
+      , `action=datax_action&emethod=worker_desc&targetName=${this.dto.processMeta.targetName}`)
+      .then((r) => {
+        if (r.success) {
+          let rList = Descriptor.wrapDescriptors(r.bizresult.pluginDesc);
 
-        let e = new SavePluginEvent();
-        e.notShowBizMsg = true;
-      e.overwriteHttpHeaderOfAppName(this.dto.processMeta.targetName);
-       // e.serverForward = "coredefine:datax_action:save_datax_worker";
-       // e.postPayload = {"k8sSpec": this.dto.powderJobServerRCSpec};
-       //  let appTisService: TISService = this.tisService;
-       //  appTisService.currentApp = new CurrentCollection(0, );
-       //  e.basicModule = this;
-        this.savePlugin.emit(e);
-    }
+          let desc = Array.from(rList.values());
+          let powerjobServer = desc.find((dec) => PowerjobCptType.Server.toString() === dec.displayName);
+          let powerjobWorker = desc.find((dec) => PowerjobCptType.Worker.toString() === dec.displayName);
+          let jobTpl = desc.find((dec) => PowerjobCptType.JobTpl.toString() == dec.displayName);
+          if (!powerjobServer) {
+            throw new Error("powerjobServer can not be null");
+          }
+          if (!powerjobWorker) {
+            throw new Error("powerjobWorker can not be null");
+          }
+          if (!jobTpl) {
+            throw new Error("jobTpl can not be null");
+          }
 
-    protected initialize(app: CurrentCollection): void {
-    }
+          this.dto.powderJobServerHetero = PluginsComponent.pluginDesc(powerjobServer, this.pluginCategory);
+          this.dto.powderJobWorkerHetero = PluginsComponent.pluginDesc(powerjobWorker, this.pluginCategory);
+          this.dto.powderjobJobTplHetero = PluginsComponent.pluginDesc(jobTpl, this.pluginCategory);
 
-    ngAfterViewInit() {
-    }
-
-
-    ngOnInit(): void {
-
-        if (this.dto.containPowerJob) {
-            return;
         }
+      });
 
-        this.httpPost('/coredefine/corenodemanage.ajax'
-            , `action=datax_action&emethod=worker_desc&targetName=${this.dto.processMeta.targetName}`)
-            .then((r) => {
-                if (r.success) {
-                    let rList = Descriptor.wrapDescriptors(r.bizresult.pluginDesc);
+  }
 
-                    let desc = Array.from(rList.values());
-                    let powerjobServer = desc.find((dec) => PowerjobCptType.Server.toString() === dec.displayName);
-                    let powerjobWorker = desc.find((dec) => PowerjobCptType.Worker.toString() === dec.displayName);
-                    let jobTpl = desc.find((dec) => PowerjobCptType.JobTpl.toString() == dec.displayName);
-                    if (!powerjobServer) {
-                        throw new Error("powerjobServer can not be null");
-                    }
-                    if (!powerjobWorker) {
-                        throw new Error("powerjobWorker can not be null");
-                    }
-                    if (!jobTpl) {
-                        throw new Error("jobTpl can not be null");
-                    }
-
-                    this.dto.powderJobServerHetero = PluginsComponent.pluginDesc(powerjobServer, this.pluginCategory);
-                    this.dto.powderJobWorkerHetero = PluginsComponent.pluginDesc(powerjobWorker, this.pluginCategory);
-                    this.dto.powderjobJobTplHetero = PluginsComponent.pluginDesc(jobTpl, this.pluginCategory);
-
-                }
-            });
-
+  afterSaveReader(e: PluginSaveResponse) {
+    if (e.saveSuccess) {
+      this.nextStep.emit(this.dto);
     }
-
-    afterSaveReader(e: PluginSaveResponse) {
-        if (e.saveSuccess) {
-            this.nextStep.emit(this.dto);
-        }
-    }
+  }
 
 
-    prestep() {
-  this.preStep.emit(this.dto);
-    }
+  prestep() {
+    this.preStep.emit(this.dto);
+  }
 }
 
