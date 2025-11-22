c33778b599cd7a69b186477be7ac7f4091c1932f
baisui1981 <baisui@qlangtech.com>
2025-11-22 17:25:59 +0800

    optimize the tis navigate header

--- src/common/navigate.bar.component.ts
+++ src/common/navigate.bar.component.ts
@@ -47,121 +47,129 @@ import {openParamsCfg} from "./plugins.component";
   template: `
     <div class="logo" [ngSwitch]="appHasNotDefine">
       <a *ngSwitchCase="true" class="navbar-brand" routerLink="/">
-        <svg version="1.1"
-             preserveAspectRatio="xMinYMin meet"
-             xmlns="http://www.w3.org/2000/svg"
-             width="50" height="31"
-             xmlns:xlink="http://www.w3.org/1999/xlink">
-
-          <image xlink:href="/images/icon/tis-log.svg" width="50" height="31"/>
-        </svg>
+        <img src="/images/icon/tis-log.svg" width="42" height="26" alt="TIS Logo"/>
       </a>
       <ng-container *ngSwitchCase="false">
         <a class="navbar-brand" routerLink="/base/applist">
-          <i class="fa fa-home fa-2x" aria-hidden="true"></i></a>
+          <i class="fa fa-home home-icon" aria-hidden="true"></i></a>
       </ng-container>
     </div>
 
     <ul class="nav-items" nz-menu nzTheme="dark" nzMode="horizontal" [ngSwitch]="appHasNotDefine">
-      <ng-container *ngSwitchCase="true">
-        <li nz-menu-item>
-          <a nz-dropdown [nzDropdownMenu]="myIndex">
-            我的管道
-            <i nz-icon nzType="down"></i>
+      <div class="nav-menu-group">
+        <ng-container *ngSwitchCase="true">
+          <li nz-menu-item>
+            <a nz-dropdown [nzDropdownMenu]="myIndex">
+              我的管道
+              <i nz-icon nzType="down"></i>
+            </a>
+            <nz-dropdown-menu #myIndex="nzDropdownMenu">
+              <ul nz-menu nzSelectable>
+                <li nz-menu-item><a routerLink="/base/applist"><i class="fa fa-list-ul"
+                                                                  aria-hidden="true"></i>列表</a></li>
+                <li nz-menu-item><a routerLink="/base/dataxadd"><i class="fa fa-plus" aria-hidden="true"></i>添加</a></li>
+              </ul>
+            </nz-dropdown-menu>
+          </li>
+          <li nz-menu-item>
+            <a nz-dropdown [nzDropdownMenu]="baseManage">
+              基础管理
+              <i nz-icon nzType="down"></i>
+            </a>
+            <nz-dropdown-menu #baseManage="nzDropdownMenu">
+              <ul nz-menu nzSelectable>
+                <li nz-menu-item><a routerLink="/base/departmentlist">业务线</a></li>
+                <li nz-menu-item><a routerLink="/base/user-profile">会员</a></li>
+                <li nz-menu-item><a routerLink="/base/datax-worker">DataX执行器</a></li>
+                <li nz-menu-item><a routerLink="/base/flink-cluster-list">Flink Cluster</a></li>
+                <li nz-menu-item><a routerLink="/base/basecfg">插件配置</a></li>
+                <!--              <li nz-menu-item><a routerLink="/base/tpl/snapshotset">索引模版</a></li>-->
+                <li nz-menu-item><a routerLink="/base/operationlog">操作日志</a></li>
+                <li nz-menu-item><a routerLink="/base/sys-errors">系统异常</a></li>
+                <li nz-menu-item><a href="javascript:void()" (click)="openLicense()">License</a></li>
+                <li nz-menu-item><a href="javascript:void()" (click)="openFreshManReadme()">新人指南</a></li>
+              </ul>
+            </nz-dropdown-menu>
+          </li>
+
+          <li nz-menu-item>
+            <a nz-dropdown [nzDropdownMenu]="offlineManage">
+              离线数据
+              <i nz-icon nzType="down"></i>
+            </a>
+            <nz-dropdown-menu #offlineManage="nzDropdownMenu">
+              <ul nz-menu nzSelectable>
+                <li nz-menu-item><a routerLink="/offline/ds">数据源管理</a></li>
+                <li nz-menu-item><a routerLink="/offline/wf">数据流分析（EMR）</a></li>
+              </ul>
+            </nz-dropdown-menu>
+          </li>
+        </ng-container>
+        <ng-container *ngSwitchCase="false">
+
+          <li class="index-select-block" nz-menu-item nzMatchRouter>
+            <nz-select name="selectedCollection"
+                       style="width: 100%;"
+                       [compareWith]="selectedCollectionCompareFn"
+                       [nzSize]="'large'"
+                       [ngModel]="app"
+                       nzPlaceHolder="请选择"
+                       [nzDropdownMatchSelectWidth]="false"
+                       nzShowSearch
+                       (ngModelChange)="onCollectionChange($event)"
+                       [nzServerSearch]="true"
+                       (nzOnSearch)="onCollectionSearch($event)"
+            >
+              <ng-container *ngFor="let o of collectionOptionList">
+                <nz-option *ngIf="!isLoading" [nzValue]="o" [nzLabel]="o.name"></nz-option>
+              </ng-container>
+              <nz-option *ngIf="isLoading" nzDisabled nzCustomContent>
+                <i nz-icon nzType="loading" class="loading-icon"></i> Loading...
+              </nz-option>
+            </nz-select>
+          </li>
+        </ng-container>
+      </div>
+
+      <div class="nav-toolbar-group">
+        <li class="nav-toolbar-item tis-about">
+          <button nz-button nzType="link" class="about-btn" (click)="openTisAbout()">
+            <i nz-icon nzType="info-circle" nzTheme="outline"></i>
+            <span>关于</span>
+          </button>
+        </li>
+        <li class="nav-toolbar-item user-profile">
+          <a class="github-stars" nz-tooltip [nzTooltipTitle]="tisStarsTpl" nzTooltipTrigger="click">
+            <i nz-icon nzType="github" nzTheme="outline"></i>
+            <img src="https://img.shields.io/github/stars/datavane/tis.svg" alt="GitHub Stars"/>
           </a>
-          <nz-dropdown-menu #myIndex="nzDropdownMenu">
-            <ul nz-menu nzSelectable>
-              <li nz-menu-item><a routerLink="/base/applist"><i class="fa fa-list-ul"
-                                                                aria-hidden="true"></i>列表</a></li>
-              <li nz-menu-item><a routerLink="/base/dataxadd"><i class="fa fa-plus" aria-hidden="true"></i>添加</a></li>
+          <ng-template #tisStarsTpl>
+            <div class="stars-tooltip">
+              <p>如果觉得TIS对您有帮助，请加一个小星星吧</p>
+              <a href="https://github.com/datavane/tis" target="_blank">https://github.com/datavane/tis</a>
+            </div>
+          </ng-template>
+        </li>
+        <li class="nav-toolbar-item user-menu">
+          <button nz-button nzType="text" nz-dropdown [nzDropdownMenu]="user" class="user-btn">
+            <i nz-icon nzType="user" nzTheme="outline"></i>
+            <span class="user-name">{{userProfile?.name}}</span>
+            <i nz-icon nzType="down" class="dropdown-icon"></i>
+          </button>
+          <nz-dropdown-menu #user="nzDropdownMenu">
+            <ul nz-menu>
+              <li nz-menu-item (click)="viewProfile()">
+                <i nz-icon nzType="info" nzTheme="outline"></i>
+                <span>信息</span>
+              </li>
+              <li nz-menu-item (click)="logout()">
+                <i nz-icon nzType="logout" nzTheme="outline"></i>
+                <span>退出</span>
+              </li>
             </ul>
           </nz-dropdown-menu>
         </li>
-        <li nz-menu-item>
-          <a nz-dropdown [nzDropdownMenu]="baseManage">
-            基础管理
-            <i nz-icon nzType="down"></i>
-          </a>
-          <nz-dropdown-menu #baseManage="nzDropdownMenu">
-            <ul nz-menu nzSelectable>
-              <li nz-menu-item><a routerLink="/base/departmentlist">业务线</a></li>
-              <li nz-menu-item><a routerLink="/base/user-profile">会员</a></li>
-              <li nz-menu-item><a routerLink="/base/datax-worker">DataX执行器</a></li>
-              <li nz-menu-item><a routerLink="/base/flink-cluster-list">Flink Cluster</a></li>
-              <li nz-menu-item><a routerLink="/base/basecfg">插件配置</a></li>
-              <!--              <li nz-menu-item><a routerLink="/base/tpl/snapshotset">索引模版</a></li>-->
-              <li nz-menu-item><a routerLink="/base/operationlog">操作日志</a></li>
-              <li nz-menu-item><a routerLink="/base/sys-errors">系统异常</a></li>
-              <li nz-menu-item><a href="javascript:void()" (click)="openLicense()">License</a></li>
-              <li nz-menu-item><a href="javascript:void()" (click)="openFreshManReadme()">新人指南</a></li>
-            </ul>
-          </nz-dropdown-menu>
-        </li>
-
-        <li nz-menu-item>
-          <a nz-dropdown [nzDropdownMenu]="offlineManage">
-            离线数据
-            <i nz-icon nzType="down"></i>
-          </a>
-          <nz-dropdown-menu #offlineManage="nzDropdownMenu">
-            <ul nz-menu nzSelectable>
-              <li nz-menu-item><a routerLink="/offline/ds">数据源管理</a></li>
-              <li nz-menu-item><a routerLink="/offline/wf">数据流分析（EMR）</a></li>
-            </ul>
-          </nz-dropdown-menu>
-        </li>
-      </ng-container>
-      <ng-container *ngSwitchCase="false">
-
-        <li class="index-select-block" nz-menu-item nzMatchRouter>
-          <nz-select name="selectedCollection"
-                     style="width: 100%;"
-                     [compareWith]="selectedCollectionCompareFn"
-                     [nzSize]="'large'"
-                     [ngModel]="app"
-                     nzPlaceHolder="请选择"
-                     [nzDropdownMatchSelectWidth]="false"
-                     nzShowSearch
-                     (ngModelChange)="onCollectionChange($event)"
-                     [nzServerSearch]="true"
-                     (nzOnSearch)="onCollectionSearch($event)"
-          >
-            <ng-container *ngFor="let o of collectionOptionList">
-              <nz-option *ngIf="!isLoading" [nzValue]="o" [nzLabel]="o.name"></nz-option>
-            </ng-container>
-            <nz-option *ngIf="isLoading" nzDisabled nzCustomContent>
-              <i nz-icon nzType="loading" class="loading-icon"></i> Loading...
-            </nz-option>
-          </nz-select>
-        </li>
-      </ng-container>
-
-      <li class="user-profile" nz-menu-item nzMatchRouter>
-        <button nz-button nzType="link" (click)="openTisAbout()">关于</button>
-        <button nz-button nz-dropdown [nzDropdownMenu]="user">
-          <i nz-icon nzType="user" style="margin: 0px" nzTheme="outline"></i>{{userProfile?.name}}
-          <i nz-icon nzType="down"></i>
-        </button>
-        <nz-dropdown-menu #user="nzDropdownMenu">
-          <ul nz-menu>
-            <li nz-menu-item (click)="viewProfile()"><i nz-icon nzType="info" nzTheme="outline"></i>信息</li>
-            <li nz-menu-item (click)="logout()"><i nz-icon nzType="logout" nzTheme="outline"></i>退出</li>
-          </ul>
-        </nz-dropdown-menu>
-
-      </li>
-      <li class="user-profile" nz-menu-item nzMatchRouter>
-
-        <span nz-tooltip [nzTooltipTitle]="tisStarsTpl" nzTooltipTrigger="click">
-          <img src="https://img.shields.io/github/stars/datavane/tis.svg"/>
-        </span>
-        <ng-template #tisStarsTpl>
-          <div>
-            <div>如果觉得TIS对您有帮助，请加一个小星星吧</div>
-            <div><a href="https://github.com/datavane/tis" target="_blank">https://github.com/datavane/tis</a></div>
-          </div>
-        </ng-template>
-      </li>
+      </div>
       <!--
                 <li nz-menu-item>
                     <a class="nav-link dropdown-toggle" href="#" id="navbarUsers" data-toggle="dropdown"
@@ -192,8 +200,14 @@ import {openParamsCfg} from "./plugins.component";
     </ng-template>
   `,
   styles: [`
+    :host {
+      display: block;
+      background: #001529 !important;
+      overflow: hidden;
+    }
+
     .ng-star-inserted {
-      margin: 0
+      margin: 0;
     }
 
     .index-select-block {
@@ -201,19 +215,191 @@ import {openParamsCfg} from "./plugins.component";
     }
 
     .nav-items {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+    .nav-menu-group {
+      display: flex;
+      align-items: center;
+      flex: 0 0 auto;
+    }
+
+    .nav-toolbar-group {
+      display: flex;
+      align-items: center;
+      margin-left: auto;
     }
 
     .navbar-brand {
       font-size: 15px;
+      transition: opacity 0.3s ease;
     }
 
-    .user-profile {
-      float: right;
+    .navbar-brand:hover {
+      opacity: 0.8;
     }
 
     .logo {
-      margin: 10px 24px 0px 24px;
+      margin: 5px 24px 0 24px;
       float: left;
+      background: #001529 !important;
+      padding: 5px 0;
+    }
+
+    .logo a {
+      display: block;
+      background: transparent !important;
+      line-height: 0;
+    }
+
+    .logo img {
+      display: block;
+      background: transparent !important;
+      vertical-align: middle;
+    }
+
+    .home-icon {
+      font-size: 26px;
+      color: rgba(255, 255, 255, 0.85);
+      transition: color 0.3s ease;
+    }
+
+    .navbar-brand:hover .home-icon {
+      color: #fff;
+    }
+
+    /* 工具栏项目样式 - 移除菜单项默认样式 */
+    .nav-toolbar-item {
+      padding: 0 8px;
+      line-height: 48px;
+      cursor: default;
+      border-bottom: 2px solid transparent;
+      position: relative;
+      display: inline-flex;
+      align-items: center;
+    }
+
+    .nav-toolbar-item::before {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 50%;
+      transform: translateY(-50%);
+      height: 16px;
+      width: 1px;
+      background: rgba(255, 255, 255, 0.15);
+    }
+
+    .nav-toolbar-item:first-of-type::before {
+      display: none;
+    }
+
+    /* 关于按钮样式 */
+    .about-btn {
+      color: rgba(255, 255, 255, 0.85);
+      transition: all 0.3s ease;
+      padding: 0 8px;
+      height: 32px;
+      line-height: 32px;
+    }
+
+    .about-btn:hover {
+      color: #fff;
+      background: transparent !important;
+    }
+
+    .about-btn i {
+      margin-right: 4px;
+    }
+
+    /* GitHub Stars 样式 */
+    .github-stars {
+      display: inline-flex;
+      align-items: center;
+      gap: 6px;
+      padding: 0 8px;
+      cursor: pointer;
+      transition: all 0.3s ease;
+      color: rgba(255, 255, 255, 0.85);
+    }
+
+    .github-stars:hover {
+      transform: translateY(-1px);
+      color: #fff;
+    }
+
+    .github-stars i {
+      font-size: 14px;
+    }
+
+    .github-stars img {
+      height: 16px;
+      vertical-align: middle;
+      transition: transform 0.3s ease;
+    }
+
+    .github-stars:hover img {
+      transform: scale(1.05);
+    }
+
+    /* Stars 提示框样式 */
+    .stars-tooltip p {
+      margin-bottom: 8px;
+    }
+
+    .stars-tooltip a {
+      color: #1890ff;
+      text-decoration: underline;
+    }
+
+    .stars-tooltip a:hover {
+      color: #40a9ff;
+    }
+
+    /* 用户按钮样式 */
+    .user-btn {
+      color: rgba(255, 255, 255, 0.85);
+      padding: 0 8px;
+      height: 32px;
+      line-height: 32px;
+      transition: all 0.3s ease;
+      display: inline-flex;
+      align-items: center;
+      gap: 4px;
+    }
+
+    .user-btn:hover {
+      color: #fff;
+      background: rgba(255, 255, 255, 0.1) !important;
+    }
+
+    .user-btn .user-name {
+      max-width: 120px;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+
+    .user-btn .dropdown-icon {
+      font-size: 12px;
+      transition: transform 0.3s ease;
+    }
+
+    .user-btn:hover .dropdown-icon {
+      transform: rotate(180deg);
+    }
+
+    /* 响应式优化 */
+    @media (max-width: 768px) {
+      .user-btn .user-name {
+        max-width: 80px;
+      }
+
+      .nav-toolbar-item {
+        padding: 0 4px;
+      }
     }
   `]
 })
